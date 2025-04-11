e592d303821898a3afb91010e94368566ef3b854
kamusaibby <ark966434@gmail.com>
2025-04-11 07:39:04 +0600

    Update ping.js

--- scripts/cmds/ping.js
+++ scripts/cmds/ping.js
@@ -1,25 +1,25 @@
 module.exports = {
-  config: {
-    name: "ping",
-    aliases: ["ms"],
-    version: "1.0",
-    author: "Sandu",
-    role: 0,
-    shortDescription: {
-      en: "Displays the current ping of the bot's system."
-    },
-    longDescription: {
-      en: "Displays the current ping of the bot's system."
-    },
-    category: "system",
-    guide: {
-      en: "Use {p}ping to check the current ping of the bot's system."
-    }
-  },
-  onStart: async function ({ api, event, args }) {
-    const timeStart = Date.now();
-    await api.sendMessage("Checking Bot's ping", event.threadID);
-    const ping = Date.now() - timeStart;
-    api.sendMessage(`The current ping ${ping}ms.`, event.threadID);
-  }
+  config: {
+    name: "ping",
+    aliases: ["ms"],
+    version: "1.0",
+    author: "Sandu",
+    role: 0,
+    shortDescription: {
+      en: "Displays the current ping of the bot's system."
+    },
+    longDescription: {
+      en: "Displays the current ping of the bot's system."
+    },
+    category: "system",
+    guide: {
+      en: "Use {p}ping to check the current ping of the bot's system."
+    }
+  },
+  onStart: async function ({ api, event, args }) {
+    const timeStart = Date.now();
+    await api.sendMessage("𝗖𝗵𝗲𝗰𝗸𝗶𝗻𝗴 𝗕𝗮𝗯𝘆 𝗽𝗶𝗻𝗴", event.threadID);
+    const ping = Date.now() - timeStart;
+    api.sendMessage(`𝗕𝗮𝗯𝘆 𝗰𝘂𝗿𝗿𝗲𝗻𝘁 𝗽𝗶𝗻𝗴 ${ping} ☘︎`, event.threadID);
+  }
 };
